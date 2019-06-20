24bc7d074789b8176c1f177fed2b1dbb8dbf9b01
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-20 13:32:06 -0300

    Add project files.

---
+++ ServicoPagamentoOnline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServicoPagamentoOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
